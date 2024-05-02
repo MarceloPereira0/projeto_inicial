c6db6522a03c7eea7ec4c57f9f837b86107fb74e
Marcelo Pereira <marceloclementinopereira@gmail.com>
2024-05-02 12:45:16 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 34
+nome = "Marcelo"
+
+# Print
+print(nome)
+print(idade)
+print("1º Método",nome,idade, "Anos")
+print("2º Método " + nome + " " + str(idade) + " Anos")
+
+# Operações
+nova_idade = idade + 1
+print("Nova idade " , nova_idade)
+
+
+# Listas
+lista = ["a","b","c","d","e","f"] #1,2,3,4,5 ] - tentei assim e não foi também
+print("Lista " + str(lista))
+
+# Métodos
+
+lista_invertida = lista
+lista_invertida.reverse()
+print(lista_invertida)
+print("Lista Invertida " + str(lista_invertida))
+# Não está funcionando e não sei porque
